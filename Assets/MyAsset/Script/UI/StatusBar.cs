adcb765fdd7d7cf34827b1f6aafc05e24327d885
nemuineru <suteado16x16@gmail.com>
2025-07-31 23:24:41 +0900

    continuation of lifebar. Where the Hell of Shapes API Documentation??

--- Assets/MyAsset/Script/UI/StatusBar.cs
+++ Assets/MyAsset/Script/UI/StatusBar.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Shapes;
 using UnityEngine;
-
 public class StatusBar : MonoBehaviour
 {
+    [SerializeField]
+    Shapes.Polyline lifeBar, energyBar;
+
+
+    [SerializeField]
+    [Range(0f, 1.0f)]
+    float Health = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,6 @@ public class StatusBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //lifeBar.
     }
 }
