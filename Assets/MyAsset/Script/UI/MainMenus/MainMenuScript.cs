f5ffdb5564817b53e00b8b5c102af5ecab28b8d5
nemuineru <suteado16x16@gmail.com>
2025-08-20 23:20:24 +0900

    more about menus

--- Assets/MyAsset/Script/UI/MainMenus/MainMenuScript.cs
+++ Assets/MyAsset/Script/UI/MainMenus/MainMenuScript.cs
@@ -5,14 +5,17 @@ using TMPro;
 
 public class MainMenuScript : MonoBehaviour
 {
+    //サブメニュー. 開くオブジェクトを選択して選択して...という感じ
     [SerializeField]
-    List<TextMeshPro> menuTextObjs, gameModeTextObjs;
+    List<SubMenuComponent> menus;
 
     [SerializeField]
     AudioSource selectSnd, confirmSnd, cancelSnd;
 
     int Depth;
-    int Selected;
+
+    int selectedInputFr = 0, cancelInputFr = 0, controlInputFr = 0;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/MyAsset/Script/UI/MainMenus/SubMenuComponent.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+//サブメニュー　フォー　メインのメニュー。　要はメニュー定義オブジェクトっすね
+//深度が深くなるほど親の負担がかかるので、大メニュー事に設定.
+public class SubMenuComponent : MonoBehaviour
+{
+    [SerializeField]
+    List<SubMenuComponent> menuTextObjs;
+
+    [SerializeField]
+    TMP_Text txt;
+
+    public int subSelected;
+    public bool currentSelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    Color selCol = new Color(0xFF,0xCC,0x40);
+    Color nonSelCol = new Color(0x40,0x30,0x20);
+    
+    // Update is called once per frame
+    //選択されたさいの自分のテキストを設定
+    void Update()
+    {
+        if (currentSelected == true)
+        {
+            txt.color = selCol;
+        }
+        else
+        {
+            txt.color = nonSelCol;
+        }
+    }
+}
