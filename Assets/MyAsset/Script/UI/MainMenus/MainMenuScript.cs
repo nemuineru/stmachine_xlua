c845971e008aec016057923cb32447c2255ae704
nemuineru <suteado16x16@gmail.com>
2025-08-23 16:06:56 +0900

    Mainmenu input selecter

--- Assets/MyAsset/Script/UI/MainMenus/MainMenuScript.cs
+++ Assets/MyAsset/Script/UI/MainMenus/MainMenuScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.ComponentModel;
 
 public class MainMenuScript : MonoBehaviour
 {
@@ -17,9 +18,12 @@ public class MainMenuScript : MonoBehaviour
     [SerializeField]
     int selectedInputFr = 0, cancelInputFr = 0, controlInputFr = 0;
 
-    int prevInput = 0;
+    [SerializeField,ReadOnly(true)]
+    int prevSelectInput = 0;
 
-    float zeroinputVal = 0.5f;
+    float setInputVal = 0.75f;
+    float resetInputVal = 0.5f;
+    bool isSelectReset = true;
 
     // Start is called before the first frame update
     void Start()
@@ -32,5 +36,18 @@ public class MainMenuScript : MonoBehaviour
     {
         selectedInputFr = InputInstance.self.inputValues.MainButton_Read == 10 ? selectedInputFr + 1 : 0;
         cancelInputFr = InputInstance.self.inputValues.SubButton_Read == 10 ? cancelInputFr + 1 : 0;
+        //Digital Value of this. -1, 0, 1..
+        int yAx =
+        (int)Mathf.Sign(InputInstance.self.inputValues.MovingAxis.y) *
+        Mathf.Min(Mathf.FloorToInt(Mathf.Abs(InputInstance.self.inputValues.MovingAxis.y) / setInputVal), 1);
+
+        if (prevSelectInput != yAx && isSelectReset == true)
+        {
+            controlInputFr += yAx;
+            isSelectReset = false;
+        }
+        //reset the value on certain point
+        prevSelectInput = yAx;
+        isSelectReset = !isSelectReset || Mathf.Abs(InputInstance.self.inputValues.MovingAxis.y) < resetInputVal;
     }
 }
